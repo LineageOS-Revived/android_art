hopez13/art

Disable support for proxy method in nterp.
They do not fall into the regular baseline JIT path (ie they cannot have
profiling info), so just bail on them for now.

Test: 044-proxy, wifi-tests
Bug: 112676029
Bug: 157658616
Bug: 160543640
Change-Id: I292d85f5d6bfd0edaad1d26e53f85f3780254fd7

runtime/art_method.h
@@ -525,11 +525,14 @@ class ArtMethod final {
   }
 
   ALWAYS_INLINE void SetProfilingInfo(ProfilingInfo* info) REQUIRES_SHARED(Locks::mutator_lock_) {
-    SetDataPtrSize(info, kRuntimePointerSize);
+    SetProfilingInfoPtrSize(info, kRuntimePointerSize);
   }
 
   ALWAYS_INLINE void SetProfilingInfoPtrSize(ProfilingInfo* info, PointerSize pointer_size)
       REQUIRES_SHARED(Locks::mutator_lock_) {
+    CHECK(!IsProxyMethod());
+    CHECK(!IsNative());
+    CHECK(IsInvokable());
     SetDataPtrSize(info, pointer_size);
   }
 

test/044-proxy/src/HotProxy.java
@@ -1,0 +1,55 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import java.lang.reflect.*;
+
+public class HotProxy {
+
+  public static final String testName = "HotProxy";
+
+  interface MyInterface {
+    void voidFoo();
+  }
+
+  static void check(boolean x) {
+    if (!x) {
+      throw new AssertionError(testName + " Check failed");
+    }
+  }
+
+  static class MyInvocationHandler implements InvocationHandler {
+    public Object invoke(Object proxy, Method method, Object[] args) {
+      check(proxy instanceof Proxy);
+      check(method.getDeclaringClass() == MyInterface.class);
+      return null;
+    }
+  }
+
+  static void testProxy() {
+    MyInvocationHandler myHandler = new MyInvocationHandler();
+    MyInterface proxyMyInterface =
+        (MyInterface)Proxy.newProxyInstance(HotProxy.class.getClassLoader(),
+                                            new Class<?>[] { MyInterface.class },
+                                            myHandler);
+    // Invoke the proxy method multiple times to ensure it becomes hot and the JIT handles it.
+    for (int i = 0; i < 0xffffffff; i++) {
+      proxyMyInterface.voidFoo();
+    }
+  }
+
+  public static void main(String args[]) {
+    testProxy();
+  }
+}

LineageOS-Revived/android_art

runtime/art_method.h
@@ -530,11 +530,14 @@ class ArtMethod final {
   }
 
   ALWAYS_INLINE void SetProfilingInfo(ProfilingInfo* info) REQUIRES_SHARED(Locks::mutator_lock_) {
-    SetDataPtrSize(info, kRuntimePointerSize);
+    SetProfilingInfoPtrSize(info, kRuntimePointerSize);
   }
 
   ALWAYS_INLINE void SetProfilingInfoPtrSize(ProfilingInfo* info, PointerSize pointer_size)
       REQUIRES_SHARED(Locks::mutator_lock_) {
+    DCHECK(!IsProxyMethod());
+    DCHECK(!IsNative());
+    DCHECK(IsInvokable());
     SetDataPtrSize(info, pointer_size);
   }
 

test/044-proxy/src/HotProxy.java
@@ -1,0 +1,55 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import java.lang.reflect.*;
+
+public class HotProxy {
+
+  public static final String testName = "HotProxy";
+
+  interface MyInterface {
+    void voidFoo();
+  }
+
+  static void check(boolean x) {
+    if (!x) {
+      throw new AssertionError(testName + " Check failed");
+    }
+  }
+
+  static class MyInvocationHandler implements InvocationHandler {
+    public Object invoke(Object proxy, Method method, Object[] args) {
+      check(proxy instanceof Proxy);
+      check(method.getDeclaringClass() == MyInterface.class);
+      return null;
+    }
+  }
+
+  static void testProxy() {
+    MyInvocationHandler myHandler = new MyInvocationHandler();
+    MyInterface proxyMyInterface =
+        (MyInterface)Proxy.newProxyInstance(HotProxy.class.getClassLoader(),
+                                            new Class<?>[] { MyInterface.class },
+                                            myHandler);
+    // Invoke the proxy method multiple times to ensure it becomes hot and the JIT handles it.
+    for (int i = 0; i < 0x10000; i++) {
+      proxyMyInterface.voidFoo();
+    }
+  }
+
+  public static void main(String args[]) {
+    testProxy();
+  }
+}